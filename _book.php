cfebb2b3cc84c7b40cbc4fc1494c80044df6cb6b
LauraVEkris <laura.vanekris@student.hu.nl>
2023-12-07 13:36:12 +0100

    added ids and names to form

--- _book.php
+++ _book.php
@@ -13,20 +13,20 @@
         <div class="formB">
             <div class="title">Reserveren</div>
             <form>
-                <label>Naam:</label>
-                <input type="text"/>
-                <label>Tent:</label>
-                <input type="checkbox" />
-                <label>Camper:</label>
-                <input type="checkbox" />
-                <label>Caravan:</label>
-                <input type="checkbox" />
-                <label>Aantal personen:</label>
-                <input type="number" />
-                <label>Aankomst:</label>
-                <input type="date">
-                <label>Vertrek:</label>
-                <input type="date">
+                <label for="name">Naam:</label>
+                <input type="text" id="name" name="name"/>
+                <label for="tent">Tent:</label>
+                <input type="checkbox" id="tent" name="tent"/>
+                <label for="camper">Camper:</label>
+                <input type="checkbox" id="camper" name="camper"/>
+                <label for="caravan">Caravan:</label>
+                <input type="checkbox" id="caravan" name="caravan" />
+                <label for="quantity">Aantal personen:</label>
+                <input type="number" id="quantity" name="quantity" min="1" max="10" placeholder="0"/>
+                <label for="start">Aankomst:</label>
+                <input type="date" name="start" id="start">
+                <label for="end" >Vertrek:</label>
+                <input type="date" name="end" id="end">
                 <label>Tent opzet service:</label>
                 <input type="checkbox" />
                 <input class="submit" type="submit" />
@@ -37,5 +37,4 @@
         </div>
     </section>
 </body>
-
 </html>
