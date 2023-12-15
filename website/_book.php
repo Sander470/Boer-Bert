b40e8e6240b8d39a82f8b49eb1b031f07d4926d1
LauraVEkris <laura.vanekris@student.hu.nl>
2023-12-15 14:52:32 +0100

    almost done with cart style
    Shopping cart is almost entirely styled, buttons are annoying.

--- website/_book.php
+++ website/_book.php
@@ -42,6 +42,7 @@
                 <input class="submit" type="submit" />
             </form>
             <script>
+                // Get the element for automatic filling of the form
                 document.getElementById('bookStart').value = "<?php echo isset($_GET['bookStart']) ? htmlspecialchars($_GET['bookStart']) : ''; ?>";
                 document.getElementById('bookEnd').value = "<?php echo isset($_GET['bookEnd']) ? htmlspecialchars($_GET['bookEnd']) : ''; ?>";
                 document.getElementById('bookQuantity').value = "<?php echo isset($_GET['bookQuantity']) ? htmlspecialchars($_GET['bookQuantity']) : ''; ?>";
@@ -54,76 +55,58 @@
     <section id="cartBooking">
         <div class="bookOptions">
             Opties
-            <div class="container">
-                <div class="screen-togo">
-                    <h2>To Go Menu</h2>
-                    <ul class="menu-items">
-                        <!--    Menu Item 1    -->
-                        <li class="menu-item">
-                            <img src="./img/plate__french-fries.webp" alt="French Fries with Ketchup" class="menu-image">
-                            <div class="menu-item-dets">
-                                <p class="menu-item-heading">French Fries with Ketchup</p>
-                                <p class="g-price">$2.23</p>
-                            </div>
-                            <button class="add-button" data-title="French Fies with Ketchup" data-price="2.23">Add to Cart</button>
-                        </li>
-                        <!--    Menu Item 2    -->
-                        <li class="menu-item">
-                            <img src="./img/plate__salmon-vegetables.webp" alt="Salmon and Vegetables" class="menu-image">
-                            <div class="menu-item-dets">
-                                <p class="menu-item-heading">Salmon and Vegetables</p>
-                                <p class="g-price">$8.99</p>
-                            </div>
-                            <button class="add-button" data-title="Salmon and Vegetables" data-price="8.99">Add to Cart</button>
-                        </li>
-                        <!--    Menu Item 3    -->
-                        <li class="menu-item">
-                            <img src="./img/plate__spaghetti-meat-sauce.webp" alt="Spaghetti with Sauce" class="menu-image">
-                            <div class="menu-item-dets">
-                                <p class="menu-item-heading">Spaghetti with Sauce</p>
-                                <p class="g-price">$7.89</p>
-                            </div>
-                            <button class="add-button" data-title="Spaghetti with Sauce" data-price="7.89">Add to Cart</button>
-                        </li>
-                        <!--    Menu Item 4    -->
-                        <li class="menu-item">
-                            <img src="./img/plate__tortellini.webp" alt="Tortellini" class="menu-image">
-                            <div class="menu-item-dets">
-                                <p class="menu-item-heading">Tortellini</p>
-                                <p class="g-price">$8.99</p>
-                            </div>
-                            <button class="add-button" data-title="Tortellini" data-price="8.99">Add to Cart</button>
-                        </li>
-                        <!--    Menu Item 5    -->
-                        <li class="menu-item">
-                            <img src="./img/plate__chicken-salad.webp" alt="Chicken Salad" class="menu-image">
-                            <div class="menu-item-dets">
-                                <p class="menu-item-heading">Chicken Salad</p>
-                                <p class="g-price">$5.75</p>
-                            </div>
-                            <button class="add-button" data-title="Chicken Salad" data-price="5.75">Add to Cart</button>
-                        </li>
-                    </ul>
-                </div>
-
-                <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>
-                <script src='wwwroot/js/cart.js'></script>
+            <div class="containerCart">
+                <ul class="menu-items">
+                    <!--    Menu Item 1    -->
+                    <li class="menu-item">
+                        <div class="menu-item-dets">
+                            <p class="menu-item-heading">Plot 3, 1 week, caravan</p>
+                            <p class="g-price">€150</p>
+                        </div>
+                        <button class="add-button" data-title="Plot 3, 1 week, caravan" data-price="150">Add to Cart</button>
+                    </li>
+                    <!--    Menu Item 2    -->
+                    <li class="menu-item">
+                        <div class="menu-item-dets">
+                            <p class="menu-item-heading">Plot 3, 1 week, tent</p>
+                            <p class="g-price">€100</p>
+                        </div>
+                        <button class="add-button" data-title="Plot 3, 1 week, tent" data-price="100">Add to Cart</button>
+                    </li>
+                    <!--    Menu Item 3    -->
+                    <li class="menu-item">
+                        <div class="menu-item-dets">
+                            <p class="menu-item-heading">Plot 3, 2 weken, caravan</p>
+                            <p class="g-price">€300</p>
+                        </div>
+                        <button class="add-button" data-title="Plot 3, 2 weken, caravan" data-price="300">Add to Cart</button>
+                    </li>
+                    <!--    Menu Item 4    -->
+                    <li class="menu-item">
+                        <div class="menu-item-dets">
+                            <p class="menu-item-heading">Plot 3, 2 weken, tent</p>
+                            <p class="g-price">€200</p>
+                        </div>
+                        <button class="add-button" data-title="Plot 3, 2 weken, tent" data-price="200">Add to Cart</button>
+                    </li>
+                </ul>
             </div>
+            <!-- Javascript and JQuery -->
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>
+            <script src='wwwroot/js/cart.js'></script>
         </div>
         </div>
 
         <div class="bookCart">
             Winkelmandje
-            <div class="screen-cart">
-                <h2>Your Cart</h2>
-                <!--  Cart Items -->
-                <ul class="cart-items">
-                </ul>
+            <!--  Cart Items -->
+            <ul class="cart-items">
+            </ul>
 
-                <div class="cart-math">
-                    <p>Add items to cart</p>
-                </div>
+            <div class="cart-math">
+                <p>Add items to cart</p>
             </div>
+        </div>
     </section>
 </body>
 
--- website/wwwroot/js/cart.js
+++ website/wwwroot/js/cart.js
@@ -1,14 +1,8 @@
 let subtotal = 0;
 
-const calculateTax = subtotal => {
-  const tax = subtotal * 0.13;
-  const formattedTax = tax.toFixed(2);
-  return formattedTax;
-};
 
 const calculateTotal = subtotal => {
-  const tax = calculateTax(subtotal);
-  const total = parseFloat(subtotal) + parseFloat(tax);
+  const total = parseFloat(subtotal);
   const formattedTotal = total.toFixed(2);
   return formattedTotal;
 };
@@ -16,23 +10,20 @@ const calculateTotal = subtotal => {
 const getImgLink = title => {
   let imgLink;
   switch (title) {
-    case 'French Fies with Ketchup':
-      imgLink = 'https://assets.codepen.io/687837/plate__french-fries.png';
+    case 'Plot 3, 1 week, caravan':
+      imgLink = '';
       break;
-    case 'Salmon and Vegetables':
-      imgLink = 'https://assets.codepen.io/687837/plate__salmon-vegetables.png';
+    case 'Plot 3, 1 week, tent':
+      imgLink = '';
       break;
-    case 'Spaghetti with Sauce':
-      imgLink = 'https://assets.codepen.io/687837/plate__spaghetti-meat-sauce.png';
+    case 'Plot 3, 2 weken, caravan':
+      imgLink = '';
       break;
-    case 'Tortellini':
-      imgLink = 'https://assets.codepen.io/687837/plate__tortellini.png';
-      break;
-    case 'Chicken Salad':
-      imgLink = 'https://assets.codepen.io/687837/plate__chicken-salad.png';
+    case 'Plot 3, 2 weken, tent':
+      imgLink = '';
       break;
     default:
-      imgLink = 'https://assets.codepen.io/687837/plate__chicken-salad.png';}
+      imgLink = '';}
 
   return imgLink;
 };
@@ -44,10 +35,10 @@ $('.add-button').on('click', function () {
 
   const element = `
     <li class="cart-item">
-      <img src="${imgLink}" alt="${title}">
+      <img src="${imgLink}">
       <div class="cart-item-dets">
         <p class="cart-item-heading">${title}</p>
-        <p class="g-price">$${price}</p>
+        <p class="g-price">€${price}</p>
       </div>
     </li>
   `;
@@ -56,21 +47,18 @@ $('.add-button').on('click', function () {
   subtotal = subtotal + price;
 
   const formattedSubtotal = subtotal.toFixed(2);
-  const tax = calculateTax(subtotal);
   const total = calculateTotal(subtotal);
 
   $('.cart-math').html(`
     <p class="cart-math-item">
       <span class="cart-math-header">Subtotal:</span>
-      <span class="g-price subtotal">$${formattedSubtotal}</span>
-    </p>
-    <p class="cart-math-item">
-      <span class="cart-math-header">Tax:</span>
-      <span class="g-price tax">$${tax}</span>
+      <span class="g-price subtotal">€${formattedSubtotal}</span>
     </p>
     <p class="cart-math-item">
       <span class="cart-math-header">Total:</span>
-      <span class="g-price total">$${total}</span>
+      <span class="g-price total">€${total}</span>
+      <button type="submit" class="submit">Submit</button>
     </p>
+    
   `);
 });
