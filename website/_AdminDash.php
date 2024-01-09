ca8ed4837f45461fe9e25c71245e19c9103cfb15
LauraVEkris <laura.vanekris@student.hu.nl>
2024-01-09 13:03:13 +0100

    TI start json
    apparently this is happening through back-end but still saving it here

--- website/_AdminDash.php
+++ website/_AdminDash.php
@@ -1,1 +1,12 @@
 <button click="submitData" id="gateButton">Gate1</button>
+<script>
+  const jsonString=[{"name":"Hank","id":"1"},
+  {"name":"andrew","id":"2"},{"name":"john","id":"3"},
+  {"name":"Flintoff","id":"4"},{"name":"Greg","id":"5"},
+  {"name":"Francis","id":"6"}];
+
+  console.log(JSON.stringify(jsonString,null,3));
+        fetch("configLED.json")
+            .then(response => response.json())
+            .then(jsonObject => console.log(jsonObject));
+</script>
