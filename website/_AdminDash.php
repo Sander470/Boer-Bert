0b4253983ee0b95f984676ccaf42d99ff120fccf
LauraVEkris <laura.vanekris@student.hu.nl>
2024-01-10 12:49:38 +0100

    button can edit variable

--- website/_AdminDash.php
+++ website/_AdminDash.php
@@ -1,1 +1,29 @@
-<button click="submitData" id="gateButton">Gate1</button>
+<button onclick="submitData()" id="gateButton" value="Open">Gate1</button>
+<script>
+    var user = {
+    map: {
+        width: 785,
+        height: 791
+    },
+    image: {
+        name: "image.png",
+        size: {width:32}
+    },
+    properties:[{
+        firstName: "Bob",
+        lastName: "Jones",
+    },
+{
+    firstName: "Janet",
+    lastName: "Jones",
+}]
+};
+console.log(user)
+    var open = document.getElementById("gateButton.value")
+
+    function submitData() {
+        user.properties[1].firstName = "Jane";
+        console.log(user)
+    }
+    
+</script>
