948cd5f290916bd83ca48a9216167c15e7eed66f
LauraVEkris <laura.vanekris@student.hu.nl>
2024-01-10 15:06:33 +0100

    reads a JSON and logs it

--- website/_AdminDash.php
+++ website/_AdminDash.php
@@ -1,29 +1,34 @@
 <button onclick="submitData()" id="gateButton" value="Open">Gate1</button>
 <script>
     var user = {
-    map: {
-        width: 785,
-        height: 791
-    },
-    image: {
-        name: "image.png",
-        size: {width:32}
-    },
-    properties:[{
-        firstName: "Bob",
-        lastName: "Jones",
-    },
-{
-    firstName: "Janet",
-    lastName: "Jones",
-}]
-};
-console.log(user)
+        map: {
+            width: 785,
+            height: 791
+        },
+        image: {
+            name: "image.png",
+            size: {
+                width: 32
+            }
+        },
+        properties: [{
+                firstName: "Bob",
+                lastName: "Jones",
+            },
+            {
+                firstName: "Janet",
+                lastName: "Jones",
+            }
+        ]
+    };
+    fetch("configLED.json")
+    .then(response => response.json())
+    .then(jsonObject => console.log(jsonObject));
+    console.log(JSON.stringify(user))
     var open = document.getElementById("gateButton.value")
 
     function submitData() {
-        user.properties[1].firstName = "Jane";
-        console.log(user)
+        user.properties[1].firstName = "Bob the second";
+        console.log(JSON.stringify(user))
     }
-    
 </script>
