d3855e02b101febafc55a097c9e817b32f3a0778
LauraVEkris <laura.vanekris@student.hu.nl>
2024-01-10 10:52:56 +0100

    did a thing
    isn't actually useful, but did it quickly anyways. Made the button log the JSON

--- website/_AdminDash.php
+++ website/_AdminDash.php
@@ -1,12 +1,33 @@
-<button click="submitData" id="gateButton">Gate1</button>
+<button onclick="submitData()" id="gateButton">Gate1</button>
 <script>
-  const jsonString=[{"name":"Hank","id":"1"},
-  {"name":"andrew","id":"2"},{"name":"john","id":"3"},
-  {"name":"Flintoff","id":"4"},{"name":"Greg","id":"5"},
-  {"name":"Francis","id":"6"}];
+    const jsonString = [{
+            "name": "Hank",
+            "id": "1"
+        },
+        {
+            "name": "andrew",
+            "id": "2"
+        }, {
+            "name": "john",
+            "id": "3"
+        },
+        {
+            "name": "Flintoff",
+            "id": "4"
+        }, {
+            "name": "Greg",
+            "id": "5"
+        },
+        {
+            "name": "Francis",
+            "id": "6"
+        }
+    ];
 
-  console.log(JSON.stringify(jsonString,null,3));
+    function submitData() {
+        console.log(JSON.stringify(jsonString, null, 3));
         fetch("configLED.json")
             .then(response => response.json())
             .then(jsonObject => console.log(jsonObject));
+    }
 </script>
