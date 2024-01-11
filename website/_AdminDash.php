b4aadf6512c2026e9fe2db64d6f1cffdf3ad9733
LauraVEkris <laura.vanekris@student.hu.nl>
2024-01-11 12:36:34 +0100

    trying to edit JSON

--- website/_AdminDash.php
+++ website/_AdminDash.php
@@ -1,34 +1,25 @@
-<button onclick="submitData()" id="gateButton" value="Open">Gate1</button>
-<script>
-    var user = {
-        map: {
-            width: 785,
-            height: 791
-        },
-        image: {
-            name: "image.png",
-            size: {
-                width: 32
-            }
-        },
-        properties: [{
-                firstName: "Bob",
-                lastName: "Jones",
-            },
-            {
-                firstName: "Janet",
-                lastName: "Jones",
-            }
-        ]
-    };
-    fetch("configLED.json")
-    .then(response => response.json())
-    .then(jsonObject => console.log(jsonObject));
-    console.log(JSON.stringify(user))
-    var open = document.getElementById("gateButton.value")
+<button onclick="openSignal()" id="gateButton" value="Open">Gate1</button>
 
-    function submitData() {
-        user.properties[1].firstName = "Bob the second";
-        console.log(JSON.stringify(user))
-    }
+<?
+$jsonString = file_get_contents('configGate.json');
+$data = json_decode($jsonString, true);
+
+function openGate()
+{
+    $data[1]['firstName'] = "Bob the second";
+    $newJsonString = json_encode($data);
+file_put_contents('configGate.json', $newJsonString);
+echo "Gate is opened!";
+}
+
+?>
+<script>
+    fetch("configGate.json")
+        .then(response => response.json())
+        .then(jsonObject => console.log(jsonObject));
+
+        function openSignal() {
+        var result = '<?php openGate(); ?>'
+        document.write(result);
+}
 </script>
