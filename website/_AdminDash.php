23cb4b6a8f6662b86f9c2d8cfdcab19d3242faa6
LauraVEkris <laura.vanekris@student.hu.nl>
2024-01-12 12:54:03 +0100

    reads status from JSON

--- website/_AdminDash.php
+++ website/_AdminDash.php
@@ -2,21 +2,23 @@
     <p id="status"></p>
     <select id="gateButton" onclick="submitData()" method="post" action="gateUpdate.php">
         <option value="open">Open</option>
-        <option value="closed" selected>Closed</option>
+        <option value="closed" >Closed</option>
     </select>
 </body>
 <script>
-    fetch("configGate.json")
-        .then(response => response.json())
-        .then(jsonObject => console.log(jsonObject));
-
-    function submitData() {
-        if (isset($_POST)) {
-            $gateStatus = $_POST["gateStatus"];
-            // moet nog ff checken hoe json schrijven dus dit klopt niet maar you get the idea
-            JSON_write("configGate.json", $gateState);
-        }
+    async function logState() {
+        const response = await fetch("configGate.json");
+        const state = await response.json(["gateState"]);
+        console.log(state);
+        document.getElementById("status").innerHTML = state["gateState"];
     };
-
-    document.getElementById("status").innerHTML = gateButton.value;
+    logState();
 </script>
+<?php function submitData()
+{
+    if (isset($_POST)) {
+        $gateStatus = $_POST["gateStatus"];
+        // moet nog ff checken hoe json schrijven dus dit klopt niet maar you get the idea
+        JSON_write("configGate.json", $gateStatus);
+    }
+}; ?>
