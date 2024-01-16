03365286a20f09d3f4aa5c29133f6a5d43779e92
Sander470 <sander-grootjans@hotmail.com>
2024-01-16 09:12:24 +0100

    the book_order.php can now talk to the database

--- website/book_order.php
+++ website/book_order.php
@@ -1,14 +1,21 @@
 <?php
 use processing\Database;
-include 'processing\Database.php';
+include 'processing/Database.php';
 
 //if (isset($_POST["submit"])) {
-    echo 'welcome<br>';
+    echo 'welcome';
 
     $firstName = $_POST["firstName"];
     $infix = $_POST["infix"];
     $lastName = $_POST["lastName"];
     $birthDate = $_POST["birthDate"];
+
+    $street = $_POST["street"];
+    $houseNr = $_POST["houseNr"];
+    $postalCode = $_POST["postalCode"];
+    $city = $_POST["city"];
+    $country = $_POST["country"];
+    $phoneNr = $_POST["phoneNr"];
 
     $fieldNr = $_POST["fieldNr"];
     $placeType = $_POST["placeType"];
@@ -17,18 +24,30 @@ include 'processing\Database.php';
     $bookStart = $_POST["bookStart"];
     $bookEnd = $_POST["bookEnd"];
 
-    $street = $_POST["street"];
-    $houseNr = $_POST["houseNr"];
-    $postalCode = $_POST["postalCode"];
-    $city = $_POST["city"];
-    $country = $_POST["country"];
-
-    $phoneNr = $_POST["phoneNr"];
     $setupHelp = $_POST["setupHelp"];
 
-    var_dump($_POST);
 
-    $database = new Database('../../db.json', true);
 
+    $database = new Database('../db.json', true);
+    $database->connectDB();
+
+    $database->conn->prepare( 'INSERT INTO `user` (mail, first_name, infix, last_name, birth_date, phone, street, house_number, postal_code, city, country) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);' );
+
+    // retrieve id from `user` where mail matches $mail from the form
+    $matchId =  $database->conn->prepare( 'SELECT id FROM user WHERE mail = ?');
+    $matchId = $matchId->bind_param('s', $mail);
+    $result = $database->conn->query($matchId);
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            
+        }
+    }
+
+
+    $database->conn->prepare( 'INSERT INTO `order` (plot_id, start_date, end_date, accommodation_type, num_people, setup_help)
+        VALUES (?, ?, ?, ?, ?, ?)');
+
+    $database->disconnectDB();
 
 //}
