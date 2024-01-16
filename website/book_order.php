159cb294220a4f7a3914b236a32c0dec9c4e1e62
Sander470 <sander-grootjans@hotmail.com>
2024-01-16 13:40:28 +0100

    you can now half-way place an order with the form. lots of missing data however, and a missing email field. a lot of hardcoded data as placeholder to see it working.

--- website/book_order.php
+++ website/book_order.php
@@ -26,27 +26,67 @@ include 'processing/Database.php';
 
     $setupHelp = $_POST["setupHelp"];
 
-
+$mail = 'dummymail';
 
     $database = new Database('../db.json', true);
     $database->connectDB();
 
-    $database->conn->prepare( 'INSERT INTO `user` (mail, first_name, infix, last_name, birth_date, phone, street, house_number, postal_code, city, country) 
+    $insertUser = $database->conn->prepare( 'INSERT INTO `user` (mail, first_name, infix, last_name, birth_date, phone, street, house_number, postal_code, city, country) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);' );
+    try {
+        $insertUser->bind_param('sssssssisss', $mail, $firstName, $infix, $lastName, $birthDate, $phoneNr, $street, $houseNr, $postalCode, $city, $country);
+        $insertUser->execute();
+        echo '<br>inserted data.';
+    } catch (Exception $e) {
+        echo '<br>' . $e;
+    }
 
     // retrieve id from `user` where mail matches $mail from the form
-    $matchId =  $database->conn->prepare( 'SELECT id FROM user WHERE mail = ?');
-    $matchId = $matchId->bind_param('s', $mail);
-    $result = $database->conn->query($matchId);
-    if($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            
+    $fetchId =  $database->conn->prepare( 'SELECT id FROM user WHERE mail = ?');
+    echo '<br>prepared mailsearch';
+    $fetchId->bind_param('s', $mail);
+    echo '<br>bound param for mailsearch';
+    try {
+        $fetchId->execute();
+        $user_id = $fetchId->get_result();
+        echo '<br>executed mailsearch';
+        } catch (Exception $e) {
+        echo "<br>" . $e;
+    }
+    if($user_id->num_rows > 0) {
+        while($row = $user_id->fetch_assoc()) {
+            echo '<br>id ' . $row["id"];
+            try {
+                $insertOrder = $database->conn->prepare( 'INSERT INTO `order` (plot_id, user_id, start_date, end_date, num_people, date, setup_help, accommodation_type) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?);' );
+                echo ' prepared..';
+            } catch (Exception $e) {
+                echo '<br>' . $e;
+            }
+            try {
+                $bookStart = '2000-01-01';
+                $bookEnd = '2000-01-10';
+                $dateThing = '2000-01-01 00:00';
+                $setupHelp = 1;
+                $fieldNr = 3;
+                $insertOrder->bind_param('iississs', $fieldNr, $user_id, $bookStart, $bookEnd, $numOfPeople, $dateThing, $setupHelp, $placeType);
+                echo ' bound params..';
+            } catch (Exception $e) {
+                echo '<br>' . $e;
+            }
+            try {
+                $insertOrder->execute();
+                echo ' inserted order..';
+            } catch (Exception $e) {
+                echo '<br>' . $e;
+            }
         }
     }
 
 
-    $database->conn->prepare( 'INSERT INTO `order` (plot_id, start_date, end_date, accommodation_type, num_people, setup_help)
-        VALUES (?, ?, ?, ?, ?, ?)');
+//    $database->conn->prepare( 'INSERT INTO `order` (plot_id, start_date, end_date, accommodation_type, num_people, setup_help)
+//        VALUES (?, ?, ?, ?, ?, ?)');
+
 
     $database->disconnectDB();
 
