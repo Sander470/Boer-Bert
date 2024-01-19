d2b040c09a33bdc9339bba2dbfecd73e79582586
LauraVEkris <laura.vanekris@student.hu.nl>
2024-01-19 10:30:23 +0100

    styling 404

--- website/_404.php
+++ website/_404.php
@@ -1,6 +1,5 @@
 <div class="title">404 - page not found</div>
 <div class="pageNotfound">
-    <p>Deze pagina bestaat niet.</p>
-    <br><br>
-    <img src="wwwroot/img/cowphoto.jpg" alt="404-img">
+    <p>Deze pagina bestaat helaas niet. Controleer of de URL correct is.</p>
+    <img src="wwwroot/img/cowlogo.ico" alt="404-img">
 </div>
