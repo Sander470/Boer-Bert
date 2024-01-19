02b3a3addf32e97712648cd795b90f64864d29a6
LauraVEkris <laura.vanekris@student.hu.nl>
2024-01-19 13:31:52 +0100

    made page not redirect
    * no redirect
    * added auto reload
    * added alert

--- website/AdminDash.php
+++ website/AdminDash.php
@@ -7,7 +7,7 @@ echo "<p class='gateText'>$jsonString<p>";
 <body>
     <link rel="stylesheet" href="wwwroot/css/style.css" />
     <link rel="icon" href="wwwroot/img/cowlogo.ico" />
-    <form action="changeGateState.php" method="post" class="gateText">
+    <form id="GateID" onsubmit="return sendData()" class="gateText">
         <input type="radio" name="gateState" value="true">open</button>
         <input type="radio" name="gateState" value="false">close</button>
         <br>
@@ -23,4 +23,33 @@ echo "<p class='gateText'>$jsonString<p>";
         console.log("status gate: " + jsonData["gateState"]);
         gateStatus = jsonData["gateState"];
     };
+
+    function sendData () {
+  // (A) GET FORM DATA
+  var data = new FormData(document.getElementById("GateID"));
+  // data.append("KEY", "VALUE");
+ 
+  // (B) INIT FETCH POST
+  fetch("changeGateState.php", {
+    method: "POST",
+    body: data
+  })
+  .then(alert ("Done! Gate is now changed"))
+  .then(location.reload())
+ 
+  // (C) RETURN SERVER RESPONSE AS TEXT
+  .then(res => {
+    if (res.status != 200) { throw new Error("Bad Server Response"); }
+    return res.text();
+  })
+ 
+  // (D) SERVER RESPONSE
+  .then(res => console.log(res))
+ 
+  // (E) HANDLE ERRORS - OPTIONAL
+  .catch(err => console.error(err));
+ 
+  // (F) PREVENT FORM SUBMIT
+  return false;
+}
 </script>
--- website/changeGateState.php
+++ website/changeGateState.php
@@ -9,9 +9,6 @@
         $data["gateState"] = $gateState;
         // replace value
         file_put_contents('configGate.json', json_encode($data));
-        header("Location: /AdminDash.php");
-        $message = "Done! Gate is now $gateState";
-        echo "<script type='text/javascript'>alert('$message');</script>";
     } else {
         $message = 'Error: No data received';
         echo "<script type='text/javascript'>alert('$message');</script>";
